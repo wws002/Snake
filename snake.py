4040131fe233e139792b49e756391de1a479a465
William Smith <wsmith@guidepostmontessori.com>
2024-12-16 10:29:38 -0600

    allow snake to loop around the screen instead of ending game when snake reaches the edge

--- snake.py
+++ snake.py
@@ -90,9 +90,16 @@ def gameLoop():
                 elif event.key == pygame.K_DOWN:
                     y1_change = snake_block
                     x1_change = 0
- 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
+
+        if x1 >= dis_width:
+            x1 = 0
+        if x1 < 0:
+            x1 = dis_width
+        if y1 >= dis_height:
+            y1 = 0
+        if y1 < 0:
+            y1 = dis_height
+
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
